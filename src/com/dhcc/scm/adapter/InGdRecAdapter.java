fd53525affb299e36e86e67bb31b345033a7a1de
zhouxin <zhouxin@dhcc.com>
2015-10-15 16:35:20 +0800

    0

---
+++ src/com/dhcc/scm/adapter/InGdRecAdapter.java
@@ -0,0 +1,77 @@
+package com.dhcc.scm.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.dhcc.scm.R;
+import com.dhcc.scm.entity.InGdRec;
+
+public class InGdRecAdapter extends BaseAdapter {
+	
+	
+	private Context ctx;
+	
+	private List<InGdRec> list;
+	
+	
+	
+	public InGdRecAdapter(Context ctx, List<InGdRec> list) {
+		super();
+		this.ctx = ctx;
+		this.list = list;
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return list.size();
+	}
+
+	@Override
+	public Object getItem(int arg0) {
+		// TODO Auto-generated method stub
+		return list.get(arg0);
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		final Holder holder;
+		if(convertView==null){
+			holder=new Holder();
+			convertView=View.inflate(ctx,R.layout.activity_ingdrec_item, null);
+			holder.desc=(TextView) convertView.findViewById(R.id.ingdrec_itm_desc);
+			holder.rp=(TextView) convertView.findViewById(R.id.ingdrec_itm_manf);
+			holder.qty=(TextView) convertView.findViewById(R.id.ingdrec_itm_qty);
+			holder.batno=(TextView) convertView.findViewById(R.id.ingdrec_itm_batno);
+			holder.manf=(TextView) convertView.findViewById(R.id.ingdrec_itm_manf);
+			//使用tag存储数据
+			convertView.setTag(holder);
+		}else{
+			holder=(Holder) convertView.getTag();
+		}
+		holder.desc.setText(list.get(position).getDesc());
+		holder.rp.setText(String.valueOf(list.get(position).getRp()));
+		holder.qty.setText(String.valueOf(list.get(position).getQty()));
+		return convertView;
+	}
+	
+	
+	static class Holder {
+		TextView desc;
+		TextView rp;
+		TextView qty;
+		TextView manf;
+		TextView batno;
+	}
+}
