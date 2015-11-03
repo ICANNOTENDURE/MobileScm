4b2ddb538d2a97f376e16fa0946595e46fd0b425
zhouxin <zhouxin@dhcc.com>
2015-11-03 14:42:07 +0800

    888

--- src/com/dhcc/scm/ui/InGdRecActivity.java
+++ src/com/dhcc/scm/ui/InGdRecActivity.java
@@ -54,7 +54,7 @@ public class InGdRecActivity extends BaseActivity implements OnClickListener {
 	@FindView(id = R.id.ingdrec_search_btn, click = true)
 	private Button btnSearch;
 	
-	//777
+	//777888
 	//321
 	//条码框
 	//444
